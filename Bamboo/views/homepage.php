47ef74e42ef5232541a0a7e858a288523f46e868
Ahmed <ahmedalkhalaf@outlook.fr>
2024-12-31 14:53:04 +0100

    header + icons

--- Bamboo/views/homepage.php
+++ Bamboo/views/homepage.php
@@ -14,22 +14,27 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
     <div class="header">
-        <!-- icon hamburger -->
-        <!-- logo -->
-        <!-- icon search -->
-        <!-- icon cart -->
+        <i class="fa-solid fa-bars icon-hamburger icon-header"></i>
+        <img class="logo icon-header" src="photos/logo.webp" alt="Bamboo logo">
+        <div class="flex-right-header">
+            <i class="fa-solid fa-magnifying-glass icon-header"></i>
+            <i class="fa-solid fa-cart-shopping icon-header"></i>
+        </div>
     </div>
     <div class="banner">
         <img class="img-banner" src="https://raw.githubusercontent.com/Ak92ka/Projet_PHP_Bamboo/refs/heads/main/Bamboo/photos/Banner_1_50.webp" alt="beige couch">
+        <!-- icons slideshow -->
+        <i class="fa-solid fa-circle-chevron-right arrow arrow-right"></i>
+        <i class="fa-solid fa-circle-chevron-left arrow arrow-left"></i>
         <div class="text-banner">
             <h1 class="banner-h1">INTÉRIEUR</h1>
             <h2 class="banner-h2">Découvrez la nouvelle gamme intérieure de Bamboo</h2>
             <div class="banner-p">
-                <!-- icons slideshow -->
                 <p>Le classique rencontre la modernité,</p>
                 <p>Bamboo offre une gamme fantastique de meubles d'intérieur,</p>
                 <p>idéale pour toute maison moderne.</p>
